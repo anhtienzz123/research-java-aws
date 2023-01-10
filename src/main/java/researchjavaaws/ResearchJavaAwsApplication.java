9c1f28ee694a511b4ffe20b71a4393394a24a65f
Tien Huynh <hatien@tma.com.vn>
2023-01-10 15:54:51 +0700

    setup

---
+++ src/main/java/researchjavaaws/ResearchJavaAwsApplication.java
@@ -0,0 +1,13 @@
+package researchjavaaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchJavaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchJavaAwsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/researchjavaaws/ResearchJavaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package researchjavaaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchJavaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
